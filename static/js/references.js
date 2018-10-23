c9fd453d8da9687b64519dfd7b22845c2ec36c77
Jake Zaia <jlz2369@g.rit.edu>
2018-10-23 00:56:53 -0400

    Finalize reformat or references, (excluding images)

--- static/js/references.js
+++ static/js/references.js
@@ -1,9 +1,27 @@
+//Copy a given message to the user's clipboard
 function copyToClipboard(message) {
+  console.log(message); //for temp debugging
+
+  if (!message)
+    return;
   var textElement = document.createElement("textarea");
   textElement.value = message;
   document.body.appendChild(textElement);
   textElement.select();
-  textElement.style.display = "none";
   document.execCommand("copy");
   document.body.removeChild(textElement);
+}
+
+
+//Add event listeners to click icons such that a click will copy the appropriate information
+var refList = $(".references-list");
+for (var i = 0; i < refList.length; i++) {
+  for (var j = 0; j < refList[i].children.length; j++) {
+    refList[i].children[j].addEventListener("click",
+                                            function(e) {
+                                              console.log(e.target.title); //for temp debugging
+                                              if (e.target.title)
+                                                copyToClipboard(e.target.title);
+                                            });
+  }
 }
